1e633b06f51fe3f634000c82096ead1dde1cf16f
ZaChrome <zachrome@orange.fr>
2021-12-07 00:42:56 +0100

    refactor 1

--- tp3/TaskManager.cs
+++ tp3/TaskManager.cs
@@ -1,6 +1,11 @@
 using System;
 
-namespace tp3.TaskManager
+namespace tp3
 {
-    
+    public class TaskManager
+    {
+        public (string,string) Parse(string command){
+            return (command[0].ToString(),command.Remove(0,2));
+        }
+    }
 }
--- tp3/TaskManagerTest.cs
+++ tp3/TaskManagerTest.cs
@@ -1,7 +1,25 @@
+using System.Threading.Tasks;
 using System;
-using tp3.TaskManager;
+using Xunit;
+
+using tp3;
 namespace tp3.Test
 {
-    
-    
+    public class TaskManagerTest
+    {
+        [Fact]
+        public void AddParse()
+        {
+            // Given
+            string userInput="+ a description";
+            (string, string) correctResult=("+", "a description");
+            TaskManager manager=new TaskManager();
+
+            // When
+            (string, string)result=manager.Parse(userInput);
+            // Then
+            Xunit.Assert.True(result==correctResult);
+        }
+    }
+
 }
