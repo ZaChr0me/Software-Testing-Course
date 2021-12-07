686cda1203f35ca0e636daf8f089709ca2988372
ZaChrome <zachrome@orange.fr>
2021-12-07 01:02:19 +0100

    refactor 2

--- tp3/TaskManager.cs
+++ tp3/TaskManager.cs
@@ -1,11 +1,57 @@
+using System.ComponentModel.DataAnnotations;
 using System;
+using Xunit;
+
+using tp3;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace tp3
 {
     public class TaskManager
     {
-        public (string,string) Parse(string command){
-            return (command[0].ToString(),command.Remove(0,2));
+        
+        
+        public List<string> Tasks{get;private set;}
+        
+        public TaskManager(){
+            Tasks=new List<string>();
+        }
+
+        public (char,string) Parse(string command){
+            return (command[0],command.Remove(0,2));
+        }
+        public void AddTask(string command){
+            Tasks.Add(command);
+        }
+    }
+    public class TaskManagerTest
+    {
+        [Fact]
+        public void Parse()
+        {
+            // Given
+            string userInput="+ a description";
+            (char, string) correctResult=('+', "a description");
+            TaskManager manager=new TaskManager();
+
+            // When
+            (char, string)result=manager.Parse(userInput);
+            // Then
+            Assert.True(result==correctResult);
+        }
+        [Fact]
+        public void AddTask()
+        {
+            // Given
+            string task="a task";
+            TaskManager manager=new TaskManager();
+            List<string> correctTasks=new List<string>(){"a task"};
+            // When
+            manager.AddTask(task);
+            // Then
+            Assert.True(correctTasks.SequenceEqual(manager.Tasks));
         }
     }
 }
